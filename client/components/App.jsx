d1d54a1ba0c7cc31235240bbcef215666170adc6
James <hello@devacademy.co.nz>
2018-11-15 18:31:00 +1300

    add base React compent layout

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import Organ from './Organ'
+
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+      <h1>React development has begun!</h1>
+      <Organ />
+    </div>
   )
 }
 
---
+++ client/components/Organ.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class Organ extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render (){
+    return (
+      <div>
+        <h1>TODO, Actualy put in the organ stuff</h1>
+      </div>
+    )
+  }
+}
+
+export default Organ
+
