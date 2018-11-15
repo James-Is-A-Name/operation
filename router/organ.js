2dad83ce864f39aba41541a4e28ddff30c6fcf88
James <hello@devacademy.co.nz>
2018-11-16 09:53:03 +1300

    split router functions

--- router/organ.js
+++ router/organ.js
@@ -1,11 +1,28 @@
 let express = require("express");
 let router = express.Router();
-let db = require("../server/data/data");
+let db = require("../server/data/datainterface");
+
+
+
 
 router.get("/list", (req, res) => {
-  let id = db.getOrganId(id);
+  db
+    .getOrgans()
+    .then(organs => {
+      res.json(organs);
+    })
+    .catch(err => {
+      res.status(404).send("error", err.message);
+    });
 });
 
-router.get("./list/:id");
+router.get("./list/:id", (req,res)=>{
+  const id = rep.params.id;
+  const organs = req.body;
+  organs.description = req.body.description.then(organ => {
+    res.json(organ);
+  });
+});
+
 
 module.exports = router;
--- server/data/datainterface.js
+++ server/data/datainterface.js
@@ -1,22 +1,16 @@
 let express = require("express");
 let router = express.Router();
-let db = require("../server/data/data");
 
-router.get("/list", (req, res) => {
-  let id = db
-    .getOrgans(id)
-    .then(organs => {
-      res.json(organs);
-    })
-    .catch(err => {
-      res.status(404).send("error", err.message);
-    });
-});
 
-router.get("./list/:id");
-const id = rep.params.id;
-const organs = req.body;
-organs.description = req.body.description.then(organ => {
-  res.json(organ);
-});
-module.exports = router;
+function getOrgans(){
+
+}
+
+function getOrganId(id){
+
+}
+
+module.exports = {
+  getOrgans,
+  getOrganId
+}
