aaeaad70feee29037cff38ab8da3334741a92a9f
Rubymoyes <moyesruby@gmail.com>
2018-11-16 09:55:50 +1300

    formatting changes

--- client/api/index.js
+++ client/api/index.js
@@ -2,26 +2,22 @@ import request from "superagent";
 
 export function getOrgans() {
   return request.get('/v1/list')
-  .then(res => {
-    let organs = res.body
-    return organs
-  })
-  .catch(() => {
-    throw Error('This needs to be the organs route')
-
-  })
+    .then(res => {
+      let organs = res.body
+      return organs
+    })
+    .catch(() => {
+      throw Error('This needs to be the organs route')
+    })
 }
 
-export function getOrganById (organId) {
+export function getOrganById(organId) {
   return request.get(`/v1/list/${organId}`)
-  .then(res => {
-    let returnedId = res.body
-    return returnedId
-  })
-  .catch(() => {
-    throw Error('This needs to be the organs id route')
-  })
+    .then(res => {
+      let returnedId = res.body
+      return returnedId
+    })
+    .catch(() => {
+      throw Error('This needs to be the organs id route')
+    })
 }
-
-
-
