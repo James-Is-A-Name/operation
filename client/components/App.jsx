8f3d6edf9b972e8942c99f13f174a5b0082cb67f
James <hello@devacademy.co.nz>
2018-11-16 09:46:02 +1300

    add inital boody outline

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,19 +6,19 @@ import Organ from './Organ'
 let theBrain = {
   name:"Brain",
   desc: "The brain is a squishy thing with strange lumps",
-  img: "/Nothing.png"
+  img: "/images/Nothing.png"
 }
 
 let theLungs = {
   name:"Lungs",
   desc: "A big'ol bag o air",
-  img: "/Nothing.png"
+  img: "/images/Nothing.png"
 }
 
 let theStomach = {
   name:"Stomach",
   desc: "FEEEEEED MEEEEEEEEE",
-  img: "/Nothing.png"
+  img: "/images/Nothing.png"
 }
 
 
@@ -37,11 +37,12 @@ let local3 = {
 
 const App = () => {
   return (
-    <div>
-      <h1>React development has begun!</h1>
-      <Organ location={local1} details={theBrain}/>
-      <Organ location={local2} details={theLungs}/>
-      <Organ location={local3} details={theStomach}/>
+    <div className="mainContainer">
+      <div className="personBody">
+        <Organ location={local1} details={theBrain}/>
+        <Organ location={local2} details={theLungs}/>
+        <Organ location={local3} details={theStomach}/>
+      </div>
     </div>
   )
 }
