64eda7977bb8c78b7ea230348006fa68c7acab16
James <42189301+James-Is-A-Name@users.noreply.github.com>
2018-11-16 09:08:32 +1300

    alter routes file names

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
-let organRoutes = require("./routes/organs");
+let organRoutes = require("../router/organ");
 const server = express();
 
 server.use(express.json());
 server.use(express.static(path.join(__dirname, "./public")));
-server.use("/api", routes);
+server.use("/api", organRoutes);
 module.exports = server;
